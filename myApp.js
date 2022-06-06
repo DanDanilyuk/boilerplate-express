1918c20c866c9400daf55b32ac411ba6b8dc2ccc
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-06 16:06:54 -0600

    Add root route with message

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+app.get('', helloMessage(req, res));
+
+function helloMessage(_req, res) {
+  res.send('Hello Express');
+}
+
 console.log('Hello World');
 
  module.exports = app;

0a7ff50954f4928d3b968635e6f141f77270ecb4
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-06 16:13:17 -0600

    Fix root function

--- myApp.js
+++ myApp.js
@@ -1,11 +1,9 @@
 let express = require('express');
 let app = express();
 
-app.get('', helloMessage(req, res));
-
-function helloMessage(_req, res) {
-  res.send('Hello Express');
-}
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
 console.log('Hello World');
 

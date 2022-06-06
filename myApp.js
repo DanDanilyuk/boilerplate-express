52feb417ff667c1635b99da4a78c540e86716a81
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-06 16:19:41 -0600

    Fix index.html file sending

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.get("/", (req, res) => {
   // res.send("Hello Express");
-  res.sendFile('views/index.html');
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 console.log('Hello World');

b59060cb59025c71fc02a6243a1fcd0373074b6e
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-07 13:56:19 -0600

    Add middleware to log request info

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 
+app.use(function middleware(req, _res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
 app.get("/", (req, res) => {
   // res.send("Hello Express");
   res.sendFile(__dirname + "/public/index.html");
@@ -8,7 +13,7 @@ app.get("/", (req, res) => {
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get("/json", (req, res) => {
+app.get("/json", (req, res, next) => {
   // res.send("Hello Express");
   if (process.env.MESSAGE_STYLE == "uppercase") {
     res.json({"message": "HELLO JSON"});

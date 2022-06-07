ad4e4936577661d875ad8a428d7504941cecbd60
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-07 11:37:16 -0600

    Add env and JSON response depending on MESSAGE_STYLE

--- myApp.js
+++ myApp.js
@@ -10,7 +10,11 @@ app.use('/public', express.static(__dirname + '/public'));
 
 app.get("/json", (req, res) => {
   // res.send("Hello Express");
-  res.json({"message": "Hello json"});
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
 });
 
 console.log('Hello World');
